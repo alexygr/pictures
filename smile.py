ae21e9b1a2ae68dc2a9670aed30f8be6d370733b
algrin <alexy.grinman@outlook.com>
2022-09-13 17:12:18 +0300

    added function curve_rect

--- smile.py
+++ smile.py
@@ -1,6 +1,7 @@
 """
 Draw angry smile
 """
+import math
 
 import pygame
 
@@ -25,9 +26,35 @@ pygame.draw.circle(screen, (255, 0, 0), center=(370, 270), radius=25)
 pygame.draw.circle(screen, (0, 0, 0), center=(370, 270), radius=25, width=2)
 pygame.draw.circle(screen, (0, 0, 0), center=(370, 270), radius=13)
 
+
 # Draw eyebrow
 # Left eyebrow
-pygame.draw.polygon(screen, (0, 0, 0), points=((150, 170), (160, 160), (240, 240), (250, 250)))
+# Calkulate left brow coordinate
+def curve_rect(start_position, length, height, angle=0):
+    """
+    Calculate rectangle coordinates concerning angle
+    :param start_position: Start position of rectangle, left bottom corner
+    :param length: length of rectangle
+    :param height: height of rectangle
+    :param angle: angle of drawing rectangle concerning axis X -90 -> 90
+    :return: list of dots
+    """
+    dots = list()
+    dots.append(start_position)
+    angle = (360 + angle) % 360
+    length_x = math.cos(math.radians(angle)) * length
+    length_y = math.sin(math.radians(angle)) * length
+    dots.append((start_position[0] + length_x, start_position[1] + length_y))
+    height_x = -math.sin(math.radians(angle)) * height
+    height_y = math.cos(math.radians(angle)) * height
+    dots.append((start_position[0] + length_x + height_x, start_position[1] + length_y + height_y))
+    dots.append((start_position[0] + height_x, start_position[1] + height_y))
+    print(dots, angle)
+    return dots
+
+
+right_brow = curve_rect(start_position=(150, 230), length=150, height=10, angle=30)
+pygame.draw.polygon(screen, (0, 0, 0), right_brow)
 # Right brow
 
 
